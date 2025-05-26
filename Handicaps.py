ad7c226ebef7be363be0fea36cb2f480c85300a6
racurry1993 <66705365+racurry1993@users.noreply.github.com>
2025-05-26 17:36:50 +0000

    new app with data

--- Handicaps.py
+++ Handicaps.py
@@ -1,7 +1,35 @@
 import streamlit as st
+import pandas as pd
+import plotly.express as px
 
-st.title("My Basic Website")
-st.header("Welcome to my site")
-st.write("This is a simple website created using Streamlit on an Android tablet")
-if st.button("Click Me"):
-    st.write("You clicked the button.")
+
+st.title("Tethered AI Golf Data Analysis")
+st.header("Amateur Golf Handicap Statistics")
+
+csv_file = "Handicap Stats.csv"
+try:
+    df = pd.read_csv(csv_file)
+except FileNotFoundError:
+    st.error("CSV File not found. Please ensure 'golfers.golfers.csv' is in the correct directory.")
+    st.stop()
+
+st.subheader("Select Handicap Index")
+handicap_options = df["Handicap"]
+selected_handicap = st.selectbox("Choose a Handicap Index:", handicap_options)
+filtered_df = df[df["Handicap"] == selected_handicap]
+
+st.write(f"Data for Handicap Index: {selected_handicap}")
+
+st.dataframe(filtered_df)
+st.subheader("Statistics by Handicap Index")
+fig = px.px.bar(
+    filtered_df,
+    x=['Avg Score to Par','FIR','FIR_Left','FIR_Right'],
+    y=filtered_df.columns[1:4],
+    title=f"Statistics for Handicap Index: {selected_handicap}",
+    labels={'value': 'Statistics', 'variable': 'Category'}
+)
+
+fig.update_layout(showlegend=True, legend_title_text='Statistics')
+st.plotly_chart(fig)
+
