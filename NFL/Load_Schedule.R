e5bd04e94336ed2f01cd3333455094e1557175b9
Curry Ryan <RFO7799@hca.corpad.net>
2025-09-17 13:42:30 -0500

    adding a lot of things

--- NFL/Load_Schedule.R
+++ NFL/Load_Schedule.R
@@ -1,6 +1,8 @@
 #install.packages("nflreadr")
 #install.packages("nflreadr", repos = c("https://nflverse.r-universe.dev", getOption("repos")))
 library(nflreadr)
+setwd("C:/Users/rfo7799/Desktop/Git/TetheredAI/NFL")
 schedule <- load_schedules(2025)
 write.csv(schedule, '2025_schedule.csv', row.names = FALSE)
+
 
